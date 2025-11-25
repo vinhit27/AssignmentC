04c7437c4642fda1314ccbae22ea961c6d90fd0d
vinhit <vov815562@gmail.com>
2025-11-25 18:56:46 +0700

    assignment

---
+++ Assignment.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void bai3()
+{
+    printf("\n=== BAI 3: TINH TIEN GIO KARAOKE ===\n");
+
+    float so_gio;
+    float tong_tien = 150000.0 ;
+
+    printf("NHAP SO GIO: ");
+
+    do
+    {
+        scanf("%f", &so_gio);
+    } while (so_gio <= 0);
+
+
+    if (so_gio <= 3)
+    {
+        tong_tien = so_gio * tong_tien;
+        printf("So gio hat: %.0f\n",so_gio);
+        printf("Tong tien can thanh toan la: %.0fVND\n",tong_tien);
+    }
+    else
+    {
+
+        float gio_tiep_theo = so_gio - 3;
+
+        float tien_gio_tiep_theo = gio_tiep_theo * 100000.0 ;
+
+        tong_tien = tong_tien*3 + tien_gio_tiep_theo;
+
+        if (so_gio > 5)
+        {
+            float tien_3_gio_dau = 150000.0*3;
+            tong_tien = (so_gio-3) * 100000.0;
+            float tien_giam_gia = (so_gio-5)*100000.0*0.2;
+            tong_tien = tien_3_gio_dau + tong_tien - tien_giam_gia;
+
+            printf("\nVi so gio hat (> 5) la %f gio, ban duoc **giam gia 20%%**.\n", so_gio);
+            printf("So tien giam gia: %.0fVND\n", tien_giam_gia);
+        }
+        printf("So gio hat: %f gio\n", so_gio);
+        printf("Tong tien can thanh toan: %.0fVND\n", tong_tien);
+    }
+}
+
+
+
+
+
+
+int main()
+{
+    bai3();
+}
